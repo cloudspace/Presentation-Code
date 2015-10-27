ae16f40195b2e5b166ec50c864b6cd62e7d2308d
BeattieM <beattie@cloudspace.com>
2015-10-27 12:22:05 -0400

    Update ParseArrayToClass.java

--- Android/ParseArrayToClass.java
+++ Android/ParseArrayToClass.java
@@ -2,17 +2,16 @@
 # Parse a json array into an array of the supplied class type
 ************************************************************/
 public BaseApiModel[] parseObjectToArray(JSONArray jsonArray, Class<?> toClass) {
-	BaseApiModel[] res = new BaseApiModel[jsonArray.length()];
-	for (int i = 0; i < jsonArray.length(); i++) {
-		try {
-			BaseApiModel item = (BaseApiModel) toClass
-					.getDeclaredConstructor(JSONObject.class).newInstance(
-							jsonArray.getJSONObject(i));
-
-			res[i] = item;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	return res;
+  BaseApiModel[] res = new BaseApiModel[jsonArray.length()];
+  for (int i = 0; i < jsonArray.length(); i++) {
+    try {
+      BaseApiModel item = (BaseApiModel) toClass
+        .getDeclaredConstructor(JSONObject.class).newInstance(
+          jsonArray.getJSONObject(i));
+      res[i] = item;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  return res;
 }
